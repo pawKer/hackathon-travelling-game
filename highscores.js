0913ed8e0a3827667e7dcfae77cf0a0df11233d1
pawKer <rares.rck96@yahoo.ro>
2021-07-02 22:17:37 +0100

    Initial leaderboard

--- highscores.js
+++ highscores.js
@@ -8,8 +8,15 @@ async function submitHighScore(event) {
   document.getElementById("high-score-submit").style.display = "none";
   document.getElementById("submit-confirm").style.display = "";
 }
-function getHighScores() {
-  return "Hello score";
+async function getHighScores() {
+  const results = await db.collection("highscores").orderBy("time", "desc").limit(20).get();
+  const orderedHighScores = await results.docs.map(doc => doc.data());
+  console.log(orderedHighScores)
+  orderedHighScores.forEach(score => {
+      let listItem = document.createElement("LI");
+      listItem.innerText = score.name + " - " + score.time + "s";
+      document.getElementById("leaderboard-list").appendChild(listItem)
+  })
 }
 function reloadPage(event) {
     event.preventDefault();
